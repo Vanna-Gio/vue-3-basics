78294aeea1997dbafe34519f095a8519afb7b484
Sovanna Ra <sovann.ra277@gmail.com>
2026-01-04 05:19:48 +0700

    use composition api

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <h1>Vue 3 Basics</h1>
   <HelloWorld name="Vanna" @sayHello="handleHello"/>
+
+  <!-- 6. Composition API (core vue 3 skills) -->
+   <p>Count: {{ count }}</p>
+   <button @click="increment">+</button>
 </template>
 
 <script>
+import { ref } from 'vue';
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
@@ -16,4 +21,15 @@ export default {
     }
   }
 }
+// Composition API
+const count = ref(0);
+function increment(){
+  count.value++;
+}
+
+// Key rules:
+
+// ref() → primitive values
+
+// .value in JS, not in template
 </script>
