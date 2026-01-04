9d1bac15b61e6fefd8bd109fa56dd4dd48cd112b
Sovanna Ra <sovann.ra277@gmail.com>
2026-01-04 08:05:58 +0700

    add basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,15 @@
-<!-- ExampleComponent.vue -->
 <template>
-  <!-- Root element for the component -->
-  <div class="example-component">
-    <!-- Interpolation: display reactive data -->
-    <h1>{{ time }}</h1>
-
-    
+  <div class="app">
+    <h1>Task Tracker</h1>
   </div>
 </template>
 
-<script>
-import { ref, onMounted, onUnmounted } from 'vue'
-
-export default {
-  setup() {
-   
-    const time = ref(0);
-    let intervalId ;
-
-    onMounted(() => {
-      intervalId = setInterval(() => {
-        time.value++;
-
-      }, 1000);
-    });
-
-    onUnmounted(() => {
-      clearInterval(intervalId); //clean up timer
-      console.log('Component unmounted, interval cleared.');
-    });
-    return { time };
-    
-    
-  }
-  
-}
+<script setup>
 </script>
-
-<style scoped>
-.example-component h1 {
-  color: #42b983;
-}
+<style >
+  .app {
+    max-width: 500;
+    margin: 40px auto;
+    font-family: Arial, sans-serif;
+  }
 </style>
-
- 
