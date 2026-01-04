17fc4a51eb992e3f9c035f2d0940bacc1072b276
Sovanna Ra <sovann.ra277@gmail.com>
2026-01-04 08:30:53 +0700

    add task input

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="app">
     <h1>Task Tracker</h1>
+    <TaskInput @add-task="addTask"/>
   </div>
 </template>
 
 <script setup>
+  import { ref } from 'vue'
+  import TaskInput from './components/TaskInput.vue';
+
+  const tasks = ref([])
+
+  function addTask(title) {
+    tasks.value.push({
+      id: Date.now(),
+      title,
+      done: false
+    })
+  }
+
+
 </script>
 <style >
   .app {
---
+++ src/components/TaskInput.vue
@@ -0,0 +1,21 @@
+<template>
+    <form @submit.prevent="submitTask">
+        <input
+            v-model="title"
+            placeholder="New task"/>
+        <button>Add</button>
+    </form>
+</template>
+<script setup>
+    import { ref } from 'vue'
+
+    const title = ref('')
+    const emit = defineEmits(['add-task'])
+
+    function submitTask() {
+        if (!title.value.trim()) return
+
+        emit('add-task', title.value)
+        title.value = ''
+    }
+</script>
